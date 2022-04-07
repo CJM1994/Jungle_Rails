0083155b42f11c8cf0b85c5e2fa9c9b135c80db0
CJM1994 <connorjohnmullin@gmail.com>
2022-04-07 12:59:55 -0700

    Add create method to controller and form for creating users, can take user info and make a user with a hashed password stored in the db

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,23 @@
 class UsersController < ApplicationController
 
   def new
-    render html: 'Display Users Here'
+    
   end
 
   def create
-    
+    user = User.new(user_params)
+
+    if user.save
+      redirect_to root_path
+    else
+      redirect_to new_user_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
