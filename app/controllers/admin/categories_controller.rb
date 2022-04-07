202b4043833428fbf9ae5cd24f23756e8532b05d
CJM1994 <connorjohnmullin@gmail.com>
2022-04-07 11:23:11 -0700

    Add route logic for admin/categories CREATE

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,6 +3,7 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with :name => ENV['ADMIN_NAME'], :password => ENV['ADMIN_PASSWORD']
 
   def new
+    @category = Category.new
   end
 
   def index
@@ -10,5 +11,20 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name,
+    )
   end
 end
