dc918e48d8d85470e5d85f57d6f9ccdc146975ae
CJM1994 <connorjohnmullin@gmail.com>
2022-04-12 02:35:19 +0000

    Add full testing with rspec for user auth, changed email validation to ignore case and leading/trailing whitespace

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -50,4 +50,41 @@ RSpec.describe User, type: :model do
     end
   end
 
+  describe '.authenticate with credentials' do
+
+    it 'returns nil if email incorrect or missing' do
+      user = User.create!(name: 'Connor', email: 'connor@aol.gov', password: 'abcd1234', password_confirmation: 'abcd1234')
+      returned = User.authenticate_with_credentials('@aol.gov', 'abcd1234')
+      expect(returned).to be_nil
+    end
+
+    it 'returns nil if password incorrect or missing' do
+      user = User.create!(name: 'Connor', email: 'connor@aol.gov', password: 'abcd1234', password_confirmation: 'abcd1234')
+      returned = User.authenticate_with_credentials('connor@aol.gov', 'abc34')
+      expect(returned).to be_nil
+    end
+
+    it 'returns user object if credentials are correct' do
+      user = User.create!(name: 'Connor', email: 'connor@aol.gov', password: 'abcd1234', password_confirmation: 'abcd1234')
+      returned = User.authenticate_with_credentials('connor@aol.gov', 'abcd1234')
+      expect(returned.name).to eq('Connor')
+      expect(returned.email).to eq('connor@aol.gov')
+    end
+
+    it 'returns user object if credentials are correct, but user adds weird cases to email' do
+      user = User.create!(name: 'Connor', email: 'connor@aol.gov', password: 'abcd1234', password_confirmation: 'abcd1234')
+      returned = User.authenticate_with_credentials('coNnOR@aOl.gov', 'abcd1234')
+      expect(returned.name).to eq('Connor')
+      expect(returned.email).to eq('connor@aol.gov')
+    end
+
+    it 'returns user object if credentials are correct, but user adds extra spaces to email for some reason' do
+      user = User.create!(name: 'Connor', email: 'connor@aol.gov', password: 'abcd1234', password_confirmation: 'abcd1234')
+      returned = User.authenticate_with_credentials('                    connor@aol.gov     ', 'abcd1234')
+      expect(returned.name).to eq('Connor')
+      expect(returned.email).to eq('connor@aol.gov')
+    end
+
+  end
+
 end
