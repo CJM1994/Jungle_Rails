4e3d50cb122ffc70fab65342a6164c77d6aa1f6c
CJM1994 <connorjohnmullin@gmail.com>
2022-04-13 20:49:05 -0700

    Refactor sold out tag to be boolean method in product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,13 @@
 class Product < ActiveRecord::Base
 
+  def sold_out?
+    if self.quantity == 0
+      true
+    else
+      false
+    end
+  end
+
   monetize :price_cents, numericality: true
   mount_uploader :image, ProductImageUploader
 
